19b0a36167498dcbbedd37c996ec17bd0efa7f08
Edward Benson <edward.benson@gmail.com>
2013-02-19 23:16:25 -0500

    write out files at end

---
+++ src/pipeline/output-files.js
@@ -0,0 +1,32 @@
+/*
+ * Outputs the files from the theme object at the very end
+ */
+Jailbreak.Pipeline.OutputFiles = function(theme, opts) {
+  this.name = "Output Files";
+  this.self = this;
+};
+
+Jailbreak.Pipeline.OutputFiles.prototype.run = function(theme, pipeline) {
+  var self = this;
+
+  /*
+   * Source Files
+   */
+  var sourceDirectory = path.join(theme.directory, "sources");
+  if (! fs.existsSync(sourceDirectory)) {
+    fs.mkdirSync(sourceDirectory);
+  }
+  _.each(theme.data.sources, function(html, name) {
+    Jailbreak.Pipeline.log(self, "Writing sources/" + name + ".html");
+    try {
+      var filename = path.join(sourceDirectory, name + ".html");
+      fs.writeFileSync(filename, html, "utf8");
+    } catch (e) {
+      console.log("Could not write file", filename, e);
+    }
+  });
+
+  // TODO(eob): Write all other assets.
+
+  pipeline.advance(self, theme, { success: true });
+};
